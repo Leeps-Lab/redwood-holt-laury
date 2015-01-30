6bb08a6a34103200bf4ebf6527486fea22ac4013
Matthew Jee <matthew@marmphco.com>
2015-01-30 00:27:17 -0800

    fixes bug where admin payout controls would not display all possible options

--- admin.js
+++ admin.js
@@ -5,7 +5,7 @@ Redwood.controller("HLAdminController", ["Admin", "$scope", function(ra, $scope)
     ra.recv("payout_select_period", function(period) {
         $scope.selectedPeriod = period;
         $scope.decisionIndices = [];
-        var rowOrder = ra.get_config(1, 0).rowOrder;
+        var rowOrder = ra.get_config(period, 0).rowOrder;
         var indexCount = rowOrder ? rowOrder.length : 10;
         for (var i = 1; i <= indexCount; ++i) {
             $scope.decisionIndices.push(i);
