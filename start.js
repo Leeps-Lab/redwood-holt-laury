3f46bda40022bf64514465a8fceba5d222e3ddcb
Matthew Jee <matthew@marmphco.com>
2015-04-09 20:35:26 -0700

    removes more redundant data from messages and lottery list

--- start.js
+++ start.js
@@ -6,60 +6,52 @@ Redwood.controller("HoltLauryController", [
   "$rootScope",
   "$scope",
   "RedwoodSubject",
-  "ConfigManager", 
-  function($rootScope, $scope, rs, configManager) {
+  "ConfigManager",
+  "$q",
+  "$http",
+  function($rootScope, $scope, rs, configManager, $q, $http) {
 
-  $scope.allDecisions = [
-    {
-      text: "1/10 of $2.00, 9/10 of $1.60 or 1/10 of $3.85, 9/10 of $0.10?",
-      choice1: [{chance: 0.1, payoff: 2.00}, {chance: 0.9, payoff: 1.60}],
-      choice2: [{chance: 0.1, payoff: 3.85}, {chance: 0.9, payoff: 0.10}]
-    },
-    {
-      text: "2/10 of $2.00, 8/10 of $1.60 or 2/10 of $3.85, 8/10 of $0.10?",
-      choice1: [{chance: 0.2, payoff: 2.00}, {chance: 0.8, payoff: 1.60}],
-      choice2: [{chance: 0.2, payoff: 3.85}, {chance: 0.8, payoff: 0.10}]
-    },
-    {
-      text: "3/10 of $2.00, 7/10 of $1.60 or 3/10 of $3.85, 7/10 of $0.10?",
-      choice1: [{chance: 0.3, payoff: 2.00}, {chance: 0.7, payoff: 1.60}],
-      choice2: [{chance: 0.3, payoff: 3.85}, {chance: 0.7, payoff: 0.10}]
-    },
-    {
-      text: "4/10 of $2.00, 6/10 of $1.60 or 4/10 of $3.85, 6/10 of $0.10?",
-      choice1: [{chance: 0.4, payoff: 2.00}, {chance: 0.6, payoff: 1.60}],
-      choice2: [{chance: 0.4, payoff: 3.85}, {chance: 0.6, payoff: 0.10}]
-    },
-    {
-      text: "5/10 of $2.00, 5/10 of $1.60 or 5/10 of $3.85, 5/10 of $0.10?",
-      choice1: [{chance: 0.5, payoff: 2.00}, {chance: 0.5, payoff: 1.60}],
-      choice2: [{chance: 0.5, payoff: 3.85}, {chance: 0.5, payoff: 0.10}]
-    },
-    {
-      text: "6/10 of $2.00, 4/10 of $1.60 or 6/10 of $3.85, 4/10 of $0.10?",
-      choice1: [{chance: 0.6, payoff: 2.00}, {chance: 0.4, payoff: 1.60}],
-      choice2: [{chance: 0.6, payoff: 3.85}, {chance: 0.4, payoff: 0.10}]
-    },
-    {
-      text: "7/10 of $2.00, 3/10 of $1.60 or 7/10 of $3.85, 3/10 of $0.10?",
-      choice1: [{chance: 0.7, payoff: 2.00}, {chance: 0.3, payoff: 1.60}],
-      choice2: [{chance: 0.7, payoff: 3.85}, {chance: 0.3, payoff: 0.10}]
-    },
-    {
-      text: "8/10 of $2.00, 2/10 of $1.60 or 8/10 of $3.85, 2/10 of $0.10?",
-      choice1: [{chance: 0.8, payoff: 2.00}, {chance: 0.2, payoff: 1.60}],
-      choice2: [{chance: 0.8, payoff: 3.85}, {chance: 0.2, payoff: 0.10}]
-    },
-    {
-      text: "9/10 of $2.00, 1/10 of $1.60 or 9/10 of $3.85, 1/10 of $0.10",
-      choice1: [{chance: 0.9, payoff: 2.00}, {chance: 0.1, payoff: 1.60}],
-      choice2: [{chance: 0.9, payoff: 3.85}, {chance: 0.1, payoff: 0.10}]
-    },
-    {
-      text: "10/10 of $2.00, 0/10 of $1.60 or 10/10 of $3.85, 0/10 of $0.10",
-      choice1: [{chance: 1.0, payoff: 2.00}, {chance: 0.0, payoff: 1.60}],
-      choice2: [{chance: 1.0, payoff: 3.85}, {chance: 0.0, payoff: 0.10}]
-    }
+  $scope.defaultDecisions = [
+    [
+      [{chance: 0.1, payoff: 2.00}, {chance: 0.9, payoff: 1.60}],
+      [{chance: 0.1, payoff: 3.85}, {chance: 0.9, payoff: 0.10}]
+    ],
+    [
+      [{chance: 0.2, payoff: 2.00}, {chance: 0.8, payoff: 1.60}],
+      [{chance: 0.2, payoff: 3.85}, {chance: 0.8, payoff: 0.10}]
+    ],
+    [
+      [{chance: 0.3, payoff: 2.00}, {chance: 0.7, payoff: 1.60}],
+      [{chance: 0.3, payoff: 3.85}, {chance: 0.7, payoff: 0.10}]
+    ],
+    [
+      [{chance: 0.4, payoff: 2.00}, {chance: 0.6, payoff: 1.60}],
+      [{chance: 0.4, payoff: 3.85}, {chance: 0.6, payoff: 0.10}]
+    ],
+    [
+      [{chance: 0.5, payoff: 2.00}, {chance: 0.5, payoff: 1.60}],
+      [{chance: 0.5, payoff: 3.85}, {chance: 0.5, payoff: 0.10}]
+    ],
+    [
+      [{chance: 0.6, payoff: 2.00}, {chance: 0.4, payoff: 1.60}],
+      [{chance: 0.6, payoff: 3.85}, {chance: 0.4, payoff: 0.10}]
+    ],
+    [
+      [{chance: 0.7, payoff: 2.00}, {chance: 0.3, payoff: 1.60}],
+      [{chance: 0.7, payoff: 3.85}, {chance: 0.3, payoff: 0.10}]
+    ],
+    [
+      [{chance: 0.8, payoff: 2.00}, {chance: 0.2, payoff: 1.60}],
+      [{chance: 0.8, payoff: 3.85}, {chance: 0.2, payoff: 0.10}]
+    ],
+    [
+      [{chance: 0.9, payoff: 2.00}, {chance: 0.1, payoff: 1.60}],
+      [{chance: 0.9, payoff: 3.85}, {chance: 0.1, payoff: 0.10}]
+    ],
+    [
+      [{chance: 1.0, payoff: 2.00}, {chance: 0.0, payoff: 1.60}],
+      [{chance: 1.0, payoff: 3.85}, {chance: 0.0, payoff: 0.10}]
+    ]
   ];
   
   // bound to the users radio button selections
@@ -74,7 +66,7 @@ Redwood.controller("HoltLauryController", [
     var comprehensiveDecisions = $scope.subjectDecisions.map(function(decision, index) {
       return {
         "decision": decision,
-        "choices": [ $scope.decisions[index].choice1, $scope.decisions[index].choice2 ]
+        "choices": [ $scope.decisions[index][0], $scope.decisions[index][1] ]
       }
     });
     
@@ -91,7 +83,6 @@ Redwood.controller("HoltLauryController", [
   $scope.selectOption = function(decisionId, selection) {
     rs.trigger("hl.selected_option", {
       "treatment": $scope.config.treatment,
-      "question-text": $scope.decisions[decisionId].text,
       "question": decisionId+1,
       "selection": selection 
     });
@@ -123,13 +114,14 @@ Redwood.controller("HoltLauryController", [
       "keyColor1": "#2382D7",
       "tintColor1": "#4594DB",
       "keyColor2": "#0659A3",
-      "tintColor2": "#0B75D4"
+      "tintColor2": "#0B75D4",
+      "lotteries": null
     });
 
     // generate array of decisions specified by rowOrder
     $scope.decisions = $scope.config.rowOrder.map(function(row) {
       var index = row - 1;
-      return $scope.allDecisions[index];
+      return $scope.defaultDecisions[index];
     });
 
     $scope.maxQuestions = $scope.decisions.length;
